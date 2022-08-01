f918f5a8292ba1d575848bbcb51aae7f4b0ea2d1
Yeolyi <type101315@gmail.com>
2022-08-01 22:36:09 +0900

    2491 수정

--- 2491/yeolyi.js
+++ 2491/yeolyi.js
@@ -2,28 +2,19 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const arr = Array.from(input[1].split(' ')).map(Number);
 
-let cnt = 1;
+let cnt1 = 1;
+let cnt2 = 1;
 let ret = 1;
 
 for (let idx = 1; idx < arr.length; idx++) {
-    if (arr[idx - 1] <= arr[idx]) {
-        cnt++;
-        ret = Math.max(ret, cnt);
+    if (arr[idx - 1] < arr[idx]) {
+        ret = Math.max(ret, ++cnt1, cnt2);
+        cnt2 = 1;
+    } else if (arr[idx - 1] == arr[idx]) {
+        ret = Math.max(ret, ++cnt1, ++cnt2);
     } else {
-        ret = Math.max(ret, cnt);
-        cnt = 1;
-    }
-}
-
-cnt = 1;
-
-for (let idx = 1; idx < arr.length; idx++) {
-    if (arr[idx - 1] >= arr[idx]) {
-        cnt++;
-        ret = Math.max(ret, cnt);
-    } else {
-        ret = Math.max(ret, cnt);
-        cnt = 1;
+        ret = Math.max(ret, cnt1, ++cnt2);
+        cnt1 = 1;
     }
 }
 
