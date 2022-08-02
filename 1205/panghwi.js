8d661967052d3b2cf1151d9f3cd0458540430066
panghwi <kanean99@naver.com>
2022-08-02 13:07:51 +0900

    1026_안광휘_수정

--- 1205/panghwi.js
+++ 1205/panghwi.js
@@ -1,33 +1,13 @@
 let fs = require('fs');
-let input = fs.readFileSync('dev/stdin').toString().split('\n');
+let input = fs
+  .readFileSync(
+    '/Users/panghwi/Library/Mobile Documents/com~apple~TextEdit/Documents/예제.txt'
+  )
+  .toString()
+  .split('\n');
+// let input = fs.readFileSync('dev/stdin').toString().split('\n');
 
 const existNumber = Number(input[0].split(' ')[0]);
 const myNumber = Number(input[0].split(' ')[1]);
 const maxNumber = Number(input[0].split(' ')[2]);
-let index = 0;
-
-if (typeof input[1] !== 'undefined') {
-  const score = input[1].split(' ').map(Number);
-  if (existNumber == maxNumber) {
-    if (myNumber > score[score.length - 1]) {
-      for (let i = 0; i < existNumber; i++) {
-        if (myNumber >= score[i]) {
-          index = i + 1;
-          break;
-        }
-      }
-    } else {
-      index = -1;
-    }
-  } else {
-    for (let i = 0; i < existNumber; i++) {
-      if (myNumber >= score[i]) {
-        index = i + 1;
-        break;
-      }
-    }
-  }
-} else {
-  index = 1;
-}
-console.log(index);
+console.log(input);
