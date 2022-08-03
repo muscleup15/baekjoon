e33c886acdeb18cb19a16ffcfc37ed6b5c85f330
panghwi <kanean99@naver.com>
2022-08-03 11:43:07 +0900

    2309_안광휘_완료

---
+++ 2309/panghwi.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+let input = fs
+  .readFileSync(
+    '/Users/panghwi/Library/Mobile Documents/com~apple~TextEdit/Documents/예제.txt'
+  )
+  .toString()
+  .trim()
+  .split('\n');
+// let input = fs.readFileSync('dev/stdin').toString().split('\n');
+let total = input.map(Number);
+let totalHeight = total.reduce((acc, element) => {
+  return (acc += element);
+}, 0);
+outerFor: for (let i = 0; i < total.length; i++) {
+  innerFor: for (let j = total.length - 1; j > i; j--) {
+    if (total[i] + total[j] + 100 === totalHeight) {
+      total[i] = 0;
+      total[j] = 0;
+      break outerFor;
+    }
+  }
+}
+const result = total.sort((a, b) => a - b).filter((element) => element !== 0);
+result.map((element) => console.log(element));
