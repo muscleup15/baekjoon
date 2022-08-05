f82c80e1c3d7228b7748c88cf6a82acab65f3507
Yeolyi <type101315@gmail.com>
2022-08-05 23:44:23 +0900

    14501_이성열_개선

--- 14501/yeolyi.py
+++ 14501/yeolyi.py
@@ -1,28 +1,16 @@
-from calendar import c
-
-
 N = int(input())
 arr = []
 for i in range(N):
-    arr.append(list(map(int, input().split(' '))))
+    arr.append(list(map(int, input().split(" "))))
+ret = [0 for _ in range(N + 1)]
 
-cache = {}
+for i in reversed(range(N)):
+    days = arr[i][0]
+    cost = arr[i][1]
+    available = i + days <= N
+    if available:
+        ret[i] = max(ret[i + 1], cost + ret[i + days])
+    else:
+        ret[i] = ret[i + 1]
 
-def solve(start):
-    if start in cache:
-        return cache[start]
-    if N <= start:
-        return 0
-    ret = 0
-    for i in range(start, N):
-        length = arr[i][0]
-        nextDay = i + length
-        if N < nextDay:
-            continue
-        cost = arr[i][1]
-        val = cost + solve(nextDay)
-        ret = max(ret, val)
-    cache[start] = ret
-    return ret
-
-print(solve(0))
+print(ret[0])
