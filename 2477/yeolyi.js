dd7026763e933a33b6f61d2447def04fbbd5746c
이성열 <type101315@gmail.com>
2022-08-13 18:57:55 +0900

    2477_이성열_수정

--- 2477/yeolyi.js
+++ 2477/yeolyi.js
@@ -2,37 +2,26 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const cost = +input[0];
-const arr = input.slice(1).map(x => x.split(' ').map(x => +x));
+const arr = input.slice(1).map(x => +x.split(' ')[1]);
 
-let longestIdx = 0;
-const longest = arr.reduce((a, b, i) => {
-    if (a[1] < b[1]) {
-        longestIdx = i
-        return b;
-    } else {
-        return a;
-    }
-});
+const rotateIndex = (i, offset) => (i + offset + 6) % 6;
 
-let anotherLongestIndex;
-let temp1 = arr[(longestIdx + 1) % 6][1] ?? 0;
-let temp2 = arr[(longestIdx + 5) % 6][1] ?? 0;
-if (temp1 < temp2) {
-    anotherLongestIndex = (longestIdx + 5) % 6;
-} else {
-    anotherLongestIndex = (longestIdx + 1) % 6;
-}
+const longestIdx = arr.reduce((a, b, i) =>  arr[a] < b ? i : a, 0);
 
-const side1 = arr[longestIdx][1];
-const side2 = arr[anotherLongestIndex][1];
+const tempIdx1 = rotateIndex(longestIdx, 1);
+const tempIdx2 = rotateIndex(longestIdx, -1);
+const anotherLongestIndex = (arr[tempIdx1] ?? 0) < (arr[tempIdx2] ?? 0) ? tempIdx2 : tempIdx1;
+
+const side1 = arr[longestIdx];
+const side2 = arr[anotherLongestIndex];
 const fullArea = side1 * side2;
 
-if ((anotherLongestIndex + 1) % 6 === longestIdx) {
-    const side3 = arr[(longestIdx + 2) % 6][1];
-    const side4 = arr[(longestIdx + 3) % 6][1];
+if (rotateIndex(anotherLongestIndex, 1) === longestIdx) {
+    const side3 = arr[rotateIndex(longestIdx, 2)];
+    const side4 = arr[rotateIndex(longestIdx, 3)];
     console.log((fullArea - side3 * side4) * cost);
 } else {
-    const side3 = arr[(anotherLongestIndex + 2) % 6][1];
-    const side4 = arr[(anotherLongestIndex + 3) % 6][1];
+    const side3 = arr[rotateIndex(anotherLongestIndex, 2)];
+    const side4 = arr[rotateIndex(anotherLongestIndex, 3)];
     console.log((fullArea - side3 * side4) * cost);
 }
