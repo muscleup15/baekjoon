c450b114ca80a22f26504d4f0b30393dbee205a6
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-08-15 16:30:43 +0900

    2559_hannah_추가

--- 2559/hannah.py
+++ 2559/hannah.py
@@ -5,9 +5,11 @@ def findmax(arr, k, l):
         last_arr = last_arr + arr[i] - arr[i-k]
         if max_arr < last_arr:
             max_arr = last_arr
+        # max_arr = max(max_arr, last_arr)로 하면 코드가 깔끔할 듯!
+        
     return max_arr
 
-N, K = map(int, input().split()) # N 개의 숫자, K일
+N, K = map(int, input().split()) # N 개의 숫자, K개
 degrees = list(map(int, input().split()))
 print(findmax(degrees, K, N))
 
