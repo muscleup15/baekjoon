9a01559590a517027a1b8c0723df688904686874
panghwi <kanean99@naver.com>
2022-08-18 11:47:34 +0900

    15656_안광휘_일단완료

--- 15656/panghwi.js
+++ 15656/panghwi.js
@@ -8,21 +8,22 @@ let input = fs
   .split('\n');
 const [N, M] = input[0].split(' ').map(Number);
 let number = input[1].split(' ').map(Number);
-let arr = [];
 number = number.sort((a, b) => a - b);
 
-// function getCombination(array, selectNumber) {
-//   let result = [];
-//   if (selectNumber === 1) {
-//     return array.map((e) => [e]);
-//   }
-//   array.forEach((fixed, index, array) => {
-//     const combinations = getCombination(array, selectNumber - 1);
-//     const attached = combinations.map((arr) => [fixed, ...arr]);
-//     result.push(...attached);
-//   });
-//   return result;
-// }
-
-// const answer = getCombination(number, M);
-// answer.map((e) => console.log(...e));
+function getCombination(array, selectNumber) {
+  let result = [];
+  if (selectNumber === 1) {
+    return array.map((e) => [e]);
+  }
+  array.forEach((fixed, index, array) => {
+    const combinations = getCombination(array, selectNumber - 1);
+    const attached = combinations.map((arr) => [fixed, ...arr]);
+    result.push(...attached);
+  });
+  return result;
+}
+let answer = getCombination(number, M);
+answer = answer.reduce((accumulator, currentValue, currentIndex, array) => {
+  return accumulator + currentValue.join(' ') + '\n';
+}, '');
+console.log(answer);
